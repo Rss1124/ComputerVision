3288a6ff80a3c6677b2b909a0980d51b1b50186f
Rss1124 <604347974@qq.com>
2023-04-19 14:59:00 +0800

    1.填了向量化计算的坑: 为什么两个算法的功能一样,但在cifar-10数据集下两者生成的距离矩阵的差距会如此大? 2.添加了更多的注释

--- tutorial/ImageClassifier/KNN.py
+++ tutorial/ImageClassifier/KNN.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 class Classifier(object):
     def __init__(self):
         self.y_train = None
@@ -9,19 +10,20 @@ class Classifier(object):
 
     def train(self, X, y):
         """
-        将"训练集"和"标签"保存下来以便预测的时候做对比
+        函数功能: 将"训练集"和"标签"保存到对象中
 
-        :param X: 训练集(M * N 的二维矩阵),每一行都是一个训练样例
-        :param y: 标签(1 * M的一维矩阵),一个训练样例对应的一个标签
+        :param X: 训练集(M * N 的二维矩阵)
+        :param y: 标签集(1 * M的一维矩阵)
         """
         self.X_train = X
         self.y_train = y
 
     def predict(self, X_test, k, num_loops):
         """
-        对测试集进行预测
+        函数功能: 对测试集进行预测 \n
+        函数特点: 通过K个离"测试数据"最近的点进行投票,提高准确率
 
-        :param X_test: 测试集(M * N的二维矩阵),每一行都是一个测试用例
+        :param X_test: 测试集(M * N的二维矩阵)
         :param k: 投票的"邻居"个数
         :param num_loops: 选择你要用的方法
         :return: 返回一个ndarray(预测结果)
@@ -36,48 +38,88 @@ class Classifier(object):
         return y_pred
 
     def compute_distance_no_loops(self, X_test):
-        print("compute_distance_no_loops")
-        L2_dists = np.sum(X_test ** 2, axis=1).reshape(-1, 1) + np.sum(self.X_train ** 2, axis=1) - 2 * np.dot(X_test, self.X_train.T)
-        print(L2_dists[0])
-        print(L2_dists.shape)
+        """
+        函数功能: 计算"测试数据"到各个"训练数据"的欧氏距离(L2_Distance) \n
+        函数特点: 完全使用向量化计算(从而避免任何显性for循环)
+
+        :param X_test: 测试集(M * N的二维矩阵)
+        :return: 返回一个ndarray(包含所有"测试数据"各自到"训练数据"的欧式距离)
+        """
+        # L2_dists = np.sum(X_test**2, axis=1).reshape(-1, 1) + \
+        #            np.sum(self.X_train**2, axis=1) - \
+        #            2 * np.dot(X_test, self.X_train.T).astype(np.float64)
+        L2_dists = np.sum((X_test[:, np.newaxis, :] - self.X_train[np.newaxis, :, :]) ** 2, axis=2)
+        # Problems:
+        # 算法1:np.sum(X_test**2, axis=1).reshape(-1, 1)+np.sum(self.X_train**2, axis=1)-2*np.dot(X_test, self.X_train.T)
+        # 算法2:np.sum((X_test[:, np.newaxis, :] - self.X_train[np.newaxis, :, :]) ** 2, axis=2)
+        # 虽然两者都是计算L2距离,但是两者最终的距离矩阵可能会不一样,甚至有时候差距会非常大
+
+        # Answer:
+        # 对于算法1,可能会存在浮点数精度的问题,因为在计算过程中有很多的乘除操作特别是平方操作,如果操作不当就可能导致精度损失.
+        # 而算法2是直接利用numpy内置的函数,减少了加减乘除操作.避免了浮点数精度的问题. 如果在数据量非常大的情况下,精度损失会更加大.
+        # poi: 所以向量化计算尽量用方法二!
+
+        # 可以使用下列代码进行验证:
+        # X_test = np.random.rand(100, 10)
+        # X_train = np.random.rand(1000, 10)
+        #
+        # L2_dists_1 = np.sum(X_test ** 2, axis=1).reshape(-1, 1) + np.sum(X_train ** 2, axis=1) - 2 * np.dot(X_test,
+        #                                                                                                     X_train.T)
+        # L2_dists_2 = np.sum((X_test[:, np.newaxis, :] - X_train[np.newaxis, :, :]) ** 2, axis=2)
+        #
+        # for i in range(L2_dists_1.shape[0]):
+        #     if L2_dists_1[0][i] != L2_dists_2[0][i]:
+        #         print(i)
+        #         print(L2_dists_1[0][i])
+        #         print(L2_dists_2[0][i])
+        #         print("not equal")
+        #         break
+        # print("finish")
         return L2_dists
 
     def compute_distance_one_loops(self, X_test):
         """
-        使用一层"for循环"来计算"测试数据"到各个"训练数据"的欧氏距离(L2_Distance)
+        函数功能: 计算"测试数据"到各个"训练数据"的欧氏距离(L2_Distance) \n
+        函数特点: 使用部分向量化计算(将for循环降低到一层)
 
-        :param X_test: 测试集
-        :return: 返回一个ndarray(所有"测试数据"各自到"训练数据"的欧式距离)
+        :param X_test: 测试集(M * N的二维矩阵)
+        :return: 返回一个ndarray(包含所有"测试数据"各自到"训练数据"的欧式距离)
         """
-        print("compute_distance_one_loops")
         num_test = X_test.shape[0]
         num_train = self.X_train.shape[0]
         L2_dists = np.zeros((num_test, num_train))
         for i in range(num_test):
+            # print(str(i) + '/' + str(num_test))
             sub_distance_array = np.abs(X_test[i, :] - self.X_train)
-            L2_dists[i] = np.sum(sub_distance_array**2, axis=1)
-        print(L2_dists[0])
+            L2_dists[i] = np.sum(sub_distance_array ** 2, axis=1)
         return L2_dists
 
     def compute_distance_two_loops(self, X_test):
-        print("compute_distance_two_loops")
+        """
+        函数功能: 计算"测试数据"到各个"训练数据"的欧氏距离(L2_Distance) \n
+        函数特点: 使用两层for循环依次算出每个距离
+
+        :param X_test: 测试集(M * N的二维矩阵)
+        :return: 返回一个ndarray(包含所有"测试数据"各自到"训练数据"的欧式距离)
+        """
         num_test = X_test.shape[0]
         num_train = self.X_train.shape[0]
         L2_dists = np.zeros((num_test, num_train))
         for i in range(num_test):
+            # print(str(i) + '/' + str(num_test))
             for j in range(num_train):
-                distance = np.sum((X_test[i] - self.X_train[j])**2)
+                distance = np.sum((X_test[i] - self.X_train[j]) ** 2)
                 L2_dists[i][j] = distance
-        print(L2_dists[0])
         return L2_dists
 
     def predict_label(self, dists, k):
         """
-        选取离测试数据"欧式距离"最近的k个点,来进行投票,选取票数最多的作为最终预测结果
+        函数功能: 对测试集进行预测 \n
+        函数特点: 通过K个离"测试数据"最近的点来进行投票,提高准确率
 
-        :param dists: 测试集与数据集之间的欧氏距离
-        :param k: 要选取的邻居个数
-        :return: 返回预测的结果
+        :param dists: 欧式距离矩阵
+        :param k: 投票的人数
+        :return: 返回一个ndarray数组(标签数组)
         """
         num_test = dists.shape[0]
         y_pred = np.zeros(num_test)
@@ -95,3 +137,18 @@ class Classifier(object):
             else:
                 y_pred[i] = closest_y[0]
         return y_pred
+
+    @staticmethod
+    def time_function(f, *args):
+        """
+        函数功能: 用来检测函数的运行时间
+
+        :param f: 待检测的函数
+        :param args: 函数所需要的参数
+        :return: 具体的时间
+        """
+        import time
+        tic = time.time()
+        f(*args)
+        toc = time.time()
+        return toc - tic
