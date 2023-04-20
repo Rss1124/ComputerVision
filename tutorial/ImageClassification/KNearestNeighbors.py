901c670fd4a5a6edce5f3390b4bb103003b495ed
Rss1124 <604347974@qq.com>
2023-04-20 20:28:35 +0800

    添加了K-Fold 交叉验证

--- tutorial/ImageClassification/KNearestNeighbors.py
+++ tutorial/ImageClassification/KNearestNeighbors.py
@@ -6,15 +6,18 @@ from tutorial.ImageClassifier.KNN import Classifier
 train_batch_url = "../DataSet/cifar-10-batches-py/data_batch_1"
 test_batch_url = "../DataSet/cifar-10-batches-py/test_batch"
 
+
 def get_train_data_from_cifar10(file_url):
     with open(file_url, 'rb') as f:
         data_train_batch = pickle.load(f, encoding='bytes')
     return data_train_batch
 
+
 def get_test_data_from_cifar10(file_url):
     with open(file_url, 'rb') as f:
         data_test_batch = pickle.load(f, encoding='bytes')
     return data_test_batch
+
 
 data_batch = get_train_data_from_cifar10(train_batch_url)
 test_batch = get_test_data_from_cifar10(test_batch_url)
@@ -45,29 +48,29 @@ X_test = X_test.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).astype("uint8")
 # print(arr)
 
 """ 将部分图片数据集可视化 """
-plt.imshow(X_train[1])
-plt.title('Label: {}'.format(y_train[1]))
-plt.show()
+# plt.imshow(X_train[1])
+# plt.title('Label: {}'.format(y_train[1]))
+# plt.show()
+#
+# classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+# num_classes = len(classes)
+# samples_per_class = 7
+# for y, cls in enumerate(classes):
+#     # 笔记1: enumerate(classes)可以将y, cls组成键值对(y:cls)==>(0:plane)(1:car)(2:bird)....
+#     idxs = np.flatnonzero(y_train == y)
+#     # 笔记2: (y_train==y) 可以把"标签相同的数据"的下标提取出来,其实就是分类(相同标签的数据在一个列表中)
+#     idxs = np.random.choice(idxs, samples_per_class, replace=False)
+#     for i, idx in enumerate(idxs):
+#         plt_idx = i * num_classes + y + 1
+#         plt.subplot(samples_per_class, num_classes, plt_idx)
+#         # 笔记3: subplot()是用于在一张图中绘制多个子图的
+#         plt.imshow(X_train[idx].astype('uint8'))
+#         plt.axis('off')
+#         if i == 0:
+#             plt.title(cls)
+# plt.show()
 
-classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-num_classes = len(classes)
-samples_per_class = 7
-for y, cls in enumerate(classes):
-    # 笔记1: enumerate(classes)可以将y, cls组成键值对(y:cls)===>(0:plane)(1:car)(2:bird)....
-    idxs = np.flatnonzero(y_train == y)
-    # 笔记2: (y_train==y) 可以把"标签相同的数据"的下标提取出来,其实就是分类(相同标签的数据在一个列表中)
-    idxs = np.random.choice(idxs, samples_per_class, replace=False)
-    for i, idx in enumerate(idxs):
-        plt_idx = i * num_classes + y + 1
-        plt.subplot(samples_per_class, num_classes, plt_idx)
-        # 笔记3: subplot()是用于在一张图中绘制多个子图的
-        plt.imshow(X_train[idx].astype('uint8'))
-        plt.axis('off')
-        if i == 0:
-            plt.title(cls)
-plt.show()
-
-""" 削减数据集的规模,减轻训练时间 """
+""" 削减数据集的规模,减少训练时间 """
 num_training = 5000
 mask = list(range(num_training))
 X_train = X_train[mask]
@@ -77,22 +80,55 @@ num_test = 500
 mask = list(range(num_test))
 X_test = X_test[mask]
 y_test = y_test[mask]
-
 X_train = np.reshape(X_train, (X_train.shape[0], -1))
 X_test = np.reshape(X_test, (X_test.shape[0], -1))
 
-KNN = Classifier()
-KNN.train(X_train, y_train)
-predict_dists = KNN.predict(X_test, 1, 1)
-# print(predict_dists)
-ture_false_table = []
-for i in range(num_test):
-    if predict_dists[i] == y_test[i]:
-        ture_false_table = np.append(ture_false_table, 1)
-    else:
-        ture_false_table = np.append(ture_false_table, 0)
-# print(ture_false_table)
-print("准确率: " + str(np.count_nonzero(ture_false_table)/num_test) + "%")
+""" K-Fold 交叉验证 """
+num_folds = 5
+k_choices = [1, 3, 5, 8, 10]
+X_train_folds = np.split(X_train, indices_or_sections=num_folds, axis=0)
+y_train_folds = np.split(y_train, indices_or_sections=num_folds)
+
+k_to_accuracies = {}
+for i in range(len(k_choices)):
+    k = k_choices[i]
+    total_accuracy = 0
+    for j in range(num_folds):
+        """ 将数据集分为训练集和验证集 """
+        X_validate_test = X_train_folds[j]
+        y_validate_test = y_train_folds[j]
+        if j == 0:
+            X_validate_train = np.array(X_train_folds[j + 1:])
+            y_validate_train = np.array(y_train_folds[j + 1:])
+        elif (j > 0) & (j < num_folds - 1):
+            X_validate_train = np.append(np.array(X_train_folds[:j]), np.array(X_train_folds[j + 1:]), axis=0)
+            y_validate_train = np.append(np.array(y_train_folds[:j]), np.array(y_train_folds[j + 1:]), axis=0)
+        else:
+            X_validate_train = np.array(X_train_folds[:j])
+            y_validate_train = np.array(y_train_folds[:j])
+        X_validate_train = np.reshape(X_validate_train, (-1, X_validate_train.shape[2]))
+        y_validate_train = np.reshape(y_validate_train, (-1,))
+
+        """ 训练模型 """
+        KNN = Classifier()
+        KNN.train(X_validate_train, y_validate_train)
+
+        """ 对验证集进行预测,同时调整超参数 """
+        predict_dists = KNN.predict(X_validate_test, k, 1)
+        num_correct = np.sum(predict_dists == y_validate_test)
+        total_accuracy = total_accuracy + float(num_correct) / 1000
+    mean_accuracy = total_accuracy / num_folds
+    k_to_accuracies["k=" + str(k) + "; mean_accuracy==>"] = mean_accuracy
+
+print(k_to_accuracies)
 
 
-
+""" 对三种方法进行效率上的评估 """
+# two_loop_time = KNN.time_function(KNN.compute_distance_two_loops, X_test)
+# print('Two loop version took %f seconds' % two_loop_time)  # ==> 30.367281 seconds
+#
+# one_loop_time = KNN.time_function(KNN.compute_distance_one_loops, X_test)
+# print('One loop version took %f seconds' % one_loop_time)  # ==> 20.551801 seconds
+#
+# no_loop_time = KNN.time_function(KNN.compute_distance_no_loops, X_test)
+# print('No loop version took %f seconds' % no_loop_time)  # ==> 16 seconds
