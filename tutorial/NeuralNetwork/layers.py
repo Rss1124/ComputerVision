f9d137cdd9d655506b0458a1aeeb21b99bb67838
Rss1124 <604347974@qq.com>
2023-05-19 11:32:28 +0800

    该文件包含许多模块化(单一神经元)的代码,用于神经网络的使用
    本次提交:
    添加了新的注释,有助于理解每个模块化函数的作用
    添加了新的网络层结构(*)

--- tutorial/NeuralNetwork/layers.py
+++ tutorial/NeuralNetwork/layers.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+""" ---------------------------------------- Affine层(全连接层) ---------------------------------------- """
+# 笔记:
+# Affine层接收输入数据，并将其与权重矩阵相乘，然后添加偏置向量，这个操作可以用数学公式表示为: out = x.dot(w) + b
+# Affine层实现了线性变换，将输入数据映射到一个新的空间。
 def affine_forward(x, w, b):
     """
-    Affine层(全连接层):
-        Affine层接收输入数据，并将其与权重矩阵相乘，然后添加偏置向量，这个操作可以用数学公式表示为:
-        out = x.dot(w) + b，Affine层实现了线性变换，将输入数据映射到一个新的空间。
+    函数功能: affine的前向传播,计算前向传播的输出值,保存缓存
 
     input:
         - x: numpy数组(N, d_1, ..., d_k)
@@ -24,9 +26,7 @@ def affine_forward(x, w, b):
 
 def affine_backward(dout, cache):
     """
-    ReLU激活函数:
-        ReLU（Rectified Linear Unit）激活函数是一种非线性函数，被广泛用于神经网络中。ReLU函数的定义是f(x) = max(0, x)，
-        即将小于零的输入值变为零，保持非负输入值不变。ReLU函数引入了非线性性质，使得神经网络可以学习更加复杂的函数映射。
+    函数功能: affine的反向传播,计算当前神经元的梯度
 
     input:
         - dout: 上一层神经元的梯度(N, M)
@@ -46,9 +46,14 @@ def affine_backward(dout, cache):
     db = np.sum(dout, axis=0)
     return dx, dw, db
 
+
+""" ---------------------------------------------- ReLU激活函数 ---------------------------------------------- """
+# 笔记1:
+# ReLU（Rectified Linear Unit）激活函数是一种非线性函数，被广泛用于神经网络中。ReLU函数的定义是f(x) = max(0, x)，
+# 即将小于零的输入值变为零，保持非负输入值不变。ReLU函数引入了非线性性质，使得神经网络可以学习更加复杂的函数映射。
 def relu_forward(x):
     """
-    函数功能: relu的前向传播,计算输出值,获取缓存
+    函数功能: relu的前向传播,计算前向传播的输出值,保存缓存
 
     input:
         - x: numpy数组(N, d_1, ..., d_k)
@@ -81,6 +86,8 @@ def relu_backward(dout, cache):
     # 如果x>0则返回1, 否则返回0
     return dx
 
+
+""" ---------------------------------------------- svm_loss激活函数 ---------------------------------------------- """
 def svm_loss(x, y):
     """
     函数功能: 基于svm_loss来计算当前神经元的损失和分数矩阵的梯度
@@ -110,6 +117,8 @@ def svm_loss(x, y):
     dx /= N
     return loss, dx
 
+
+""" ---------------------------------------------- softmax激活函数 ---------------------------------------------- """
 def softmax_loss(x, y):
     """
     函数功能: 基于softmax_loss来计算当前神经元的损失和分数矩阵的梯度
@@ -141,3 +150,49 @@ def softmax_loss(x, y):
     dx = dx * exp_scores
     dx /= N
     return loss, dx
+
+
+""" ------------------------------------------ affine + relu 的网络层结构 ------------------------------------------ """
+# 笔记1:
+# affine_relu是一种常用的网络层结构,用于构建神经网络模型中的隐藏层.它由两个部分组成: Affine层和ReLU激活函数.
+# 将Affine层和ReLU激活函数结合在一起形成affine_relu层,可以在神经网络的隐藏层中引入非线性变换.这对于神经网络的表达能力非常重要,
+# 因为它允许网络学习更加复杂的特征和决策边界.ReLU激活函数还有助于缓解梯度消失问题,并提供更好的梯度传播,从而加快训练过程.
+#
+# 总结而言,affine_relu层的设计旨在引入非线性变换和激活函数,以增强神经网络的表达能力,并改善梯度传播的效果.
+# 这有助于提高神经网络的性能和学习能力.
+
+def affine_relu_forward(x, w, b):
+    """
+    函数功能: a + r 神经网络的前向传播,计算前向传播的输出值,保存缓存
+
+    input:
+        - x: 训练数据,会输入进affine
+        - w: 权重矩阵
+        - b: 偏置项
+    return:
+        - out: 从relu输出的数据
+        - cache: 缓存数据
+    """
+
+    affine_out, affine_cache = affine_forward(x, w, b)
+    out, relu_cache = relu_forward(affine_out)
+    cache = (affine_cache, relu_cache)
+    return out, cache
+
+def affine_relu_backward(dout, cache):
+    """
+    函数功能: a + r 神经网络的反向传播
+
+    input:
+        - dout: 上一层的梯度
+        - cache: 上一层的缓存
+    return:
+        - dx: affine_x的梯度
+        - dw: affine_w的梯度
+        - db: affine_b的梯度
+    """
+
+    affine_cache, relu_cache = cache
+    relu_dout = relu_backward(dout, relu_cache)
+    dx, dw, db = affine_backward(relu_dout, affine_cache)
+    return dx, dw, db
